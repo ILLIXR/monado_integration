2a716fb8605705022817d94af4e3d7f42cdc691b
Finn Sinclair <finnnorth@gmail.com>
2020-08-19 01:21:33 -0500

    Add illixr-vsync and "illixr-time" to Monado plugin.

--- src/xrt/drivers/illixr/illixr_component.cpp
+++ src/xrt/drivers/illixr/illixr_component.cpp
@@ -21,11 +21,13 @@ public:
 		, sb{pb->lookup_impl<switchboard>()}
 		, sb_pose{pb->lookup_impl<pose_prediction>()}
 		, sb_eyebuffer{sb->publish<rendered_frame_alt>("eyebuffer")}
+		, sb_vsync_estimate{sb->subscribe_latest<time_type>("vsync_estimate")}
 	{ }
 
 	const std::shared_ptr<switchboard> sb;
 	const std::shared_ptr<pose_prediction> sb_pose;
 	const std::unique_ptr<writer<rendered_frame_alt>> sb_eyebuffer;
+	const std::unique_ptr<reader_latest<time_type>> sb_vsync_estimate;
 	pose_type prev_pose; /* stores a copy of pose_type each time illixr_read_pose() is called */
 	std::chrono::time_point<std::chrono::system_clock> sample_time; /* when prev_pose was stored */
 };
@@ -75,6 +77,25 @@ extern "C" void illixr_write_frame(unsigned int left,
 	frame->texture_handles[1] = right;
 	frame->render_pose = illixr_plugin_obj->prev_pose;
 	frame->sample_time = illixr_plugin_obj->sample_time;
+	frame->render_time = std::chrono::high_resolution_clock::now();
 
 	illixr_plugin_obj->sb_eyebuffer->put(frame);
+}
+
+extern "C" int64_t illixr_get_vsync_ns() {
+	assert(illixr_plugin_obj && "illixr_plugin_obj must be initialized first.");
+
+	const time_type *vsync_estimate = illixr_plugin_obj->sb_vsync_estimate->get_latest_ro();
+	
+	if(vsync_estimate == nullptr)
+	{
+		return std::chrono::duration_cast<std::chrono::nanoseconds>((std::chrono::system_clock::now()).time_since_epoch()).count() + NANO_SEC/60;
+	}
+
+	return std::chrono::duration_cast<std::chrono::nanoseconds>((*vsync_estimate).time_since_epoch()).count();
+}
+
+extern "C" int64_t illixr_get_now_ns() {
+	assert(illixr_plugin_obj && "illixr_plugin_obj must be initialized first.");
+	return std::chrono::duration_cast<std::chrono::nanoseconds>((std::chrono::system_clock::now()).time_since_epoch()).count();
 }
