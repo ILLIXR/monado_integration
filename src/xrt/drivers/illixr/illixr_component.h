0ea0d893ad313740acbb80d736537c272b21671b
Finn Sinclair <finnnorth@gmail.com>
2020-08-19 01:22:57 -0500

    Header fixes

--- src/xrt/drivers/illixr/illixr_component.h
+++ src/xrt/drivers/illixr/illixr_component.h
@@ -9,6 +9,8 @@ struct xrt_pose illixr_read_pose();
 
 void illixr_write_frame(unsigned int left,
                         unsigned int right);
+int64_t illixr_get_vsync_ns();
+int64_t illixr_get_now_ns();
 void get_illixr_context();
 
 #ifdef __cplusplus
